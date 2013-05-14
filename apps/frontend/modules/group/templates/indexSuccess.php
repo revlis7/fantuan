a6c0517c28d8d86f6caa45562100bfa5a3f42696
revlis7 <silver47@yeah.net>
2013-05-14 18:02:50 +0800

    add user links

--- apps/frontend/modules/group/templates/indexSuccess.php
+++ apps/frontend/modules/group/templates/indexSuccess.php
@@ -1,8 +1,8 @@
 <p>Group: <strong><?php echo $group; ?></strong></p>
-<p>Captain: <strong><?php echo $captain; ?></strong></p>
+<p>Captain: <strong><a href="<?php echo url_for('@user?name='.$captain); ?>"><?php echo $captain; ?></a></strong></p>
 <p>Members: </p>
 <ul>
 <?php foreach ($members as $member): ?>
-  <li><strong><?php echo $member; ?></strong></li>
+  <li><strong><a href="<?php echo url_for('@user?name='.$member); ?>"><?php echo $member; ?></a></strong></li>
 <?php endforeach; ?>
 </ul>
