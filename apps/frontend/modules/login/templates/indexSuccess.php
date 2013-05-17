ce3f38bf7c7cf7877a4ed653d7d9fc2147774a72
revlis7 <silver47@yeah.net>
2013-05-17 16:48:05 +0800

    new looking

--- apps/frontend/modules/login/templates/indexSuccess.php
+++ apps/frontend/modules/login/templates/indexSuccess.php
@@ -1,11 +1,13 @@
+<div class="container">
 <h1>login</h1>
 <form action="<?php echo url_for('login/index') ?>" method="post">
   <table>
     <?php echo $form ?>
     <tr>
       <td colspan="2">
-        <input type="submit" />
+        <button type="submit" class="btn btn-large btn-primary">Sign in</button>
       </td>
     </tr>
   </table>
 </form>
+</div>
