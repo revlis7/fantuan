b0ce417f8f90aea1a7bff1c9be7b48d4548e9074
revlis7 <silver47@yeah.net>
2013-05-18 16:01:17 +0800

    login page updated

--- apps/frontend/modules/login/templates/indexSuccess.php
+++ apps/frontend/modules/login/templates/indexSuccess.php
@@ -1,13 +1,12 @@
 <div class="container">
-<h1>login</h1>
-<form action="<?php echo url_for('login/index') ?>" method="post">
-  <table>
-    <?php echo $form ?>
-    <tr>
-      <td colspan="2">
-        <button type="submit" class="btn btn-large btn-primary">Sign in</button>
-      </td>
-    </tr>
-  </table>
+<form action="<?php echo url_for('login/index') ?>" method="post" class="form-signin">
+  <h1>Login</h1>
+  <?php echo $form['email']->render(array('placeholder' => 'Email address', 'class' => 'input-block-level')); ?>
+  <?php echo $form['email']->renderError(); ?>
+  <?php echo $form['password']->render(array('placeholder' => 'Password', 'class' => 'input-block-level')); ?>
+  <?php echo $form['password']->renderError(); ?>
+  <?php echo $form->renderHiddenFields(); ?>
+  <?php echo $form->renderGlobalErrors(); ?>
+  <button type="submit" class="btn btn-large btn-primary">Sign in</button>
 </form>
 </div>
