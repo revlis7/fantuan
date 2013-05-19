16f0d1873292ff541257eb33e9d8bfe9bccfd074
revlis7 <silver47@yeah.net>
2013-05-19 15:55:31 +0800

    new register page looking

--- apps/frontend/modules/register/templates/_form.php
+++ apps/frontend/modules/register/templates/_form.php
@@ -1,15 +1,16 @@
-<h1>form</h1>
-<form action="<?php echo url_for('register/index') ?>" method="post">
-  <table>
-    <tfoot>
-      <tr>
-        <td colspan="2">
-          <input type="submit" value="Register" />
-        </td>
-      </tr>
-    </tfoot>
-    <tbody>
-      <?php echo $form ?>
-    </tbody>
-  </table>
-</form>
+<div class="container">
+  <form action="<?php echo url_for('register/index') ?>" method="post" class="form-signin">
+    <h1>Register</h1>
+    <?php echo $form['email']->render(array('placeholder' => 'Email address', 'class' => 'input-block-level')); ?>
+    <?php echo $form['email']->renderError(); ?>
+    <?php echo $form['name']->render(array('placeholder' => 'Your nickname', 'class' => 'input-block-level')); ?>
+    <?php echo $form['name']->renderError(); ?>
+    <?php echo $form['password']->render(array('placeholder' => 'Password', 'class' => 'input-block-level')); ?>
+    <?php echo $form['password']->renderError(); ?>
+    <?php echo $form['password_confirm']->render(array('placeholder' => 'Password confirm', 'class' => 'input-block-level')); ?>
+    <?php echo $form['password_confirm']->renderError(); ?>
+    <?php echo $form->renderHiddenFields(); ?>
+    <?php echo $form->renderGlobalErrors(); ?>
+    <button type="submit" class="btn btn-large btn-primary">Register</button>
+  </form>
+</div>
--- apps/frontend/modules/register/templates/indexSuccess.php
+++ apps/frontend/modules/register/templates/indexSuccess.php
@@ -1,2 +1,1 @@
-<h1>register</h1>
 <?php include_partial('form', array('form' => $form)) ?>
