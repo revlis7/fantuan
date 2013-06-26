f695038ad8671e8c953bf71f3dc1f539528e9181
revlis7 <silver47@yeah.net>
2013-06-26 15:37:22 +0800

    update the page

--- apps/frontend/modules/activity/templates/indexSuccess.php
+++ apps/frontend/modules/activity/templates/indexSuccess.php
@@ -1,11 +1,23 @@
-<p>活动列表: <strong><?php echo $activity; ?></strong></p>
-<p>Total Cost: <strong><?php echo sprintf('%.2f', $activity->getCost() / 100); ?></strong></p>
-<p>Members: </p>
-<ul>
-<?php foreach($members as $member): ?>
-<li>
-  <strong><a href="<?php echo url_for('@user?name='.$member); ?>"><?php echo $member; ?></a></strong> - 
-  <?php echo sprintf('%.2f', $member->getActivityCost($activity) / 100); ?>
-</li>
-<?php endforeach; ?>
-</ul>
+<div class="container">
+  <div class="row">
+    <div class="span8">
+      <p>参与团员：</p>
+      <div class="members">
+      <?php foreach($members as $member): ?>
+        <div class="member">
+          <p><strong><a href="<?php echo url_for('@user?name='.$member); ?>"><?php echo $member; ?></a></strong></p>
+          <p>个人消费了：<?php echo sprintf('%.2f', $member->getActivityCost($activity) / 100); ?></p>
+        </div>
+      <?php endforeach; ?>
+      </div>
+    </div>
+    <div class="span4">
+      <div class="profile">
+        <div>
+          <p>活动名称： <strong><?php echo $activity; ?></strong></p>
+          <p>共计消费： <strong><?php echo sprintf('%.2f', $activity->getCost() / 100); ?></strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
